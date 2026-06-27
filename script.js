5ad1a27dab05be03601f592057577dc749d97c7a
Chandini Tammana <chandinitammana4@gmail.com>
2026-06-27 17:58:05 +0530

    Connected frontend to API Gateway

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-function submitForm()
-{
-    let name =
-    document.getElementById("name").value;
+async function submitForm() {
 
-    let email =
-    document.getElementById("email").value;
+    const data = {
+        fullName: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phoneNumber: document.getElementById("phone").value,
+        qualification: document.getElementById("qualification").value,
+        experience: document.getElementById("experience").value,
+        skills: document.getElementById("skills").value,
+        coverLetter: document.getElementById("coverLetter").value
+    };
 
-    let phone =
-    document.getElementById("phone").value;
+    try {
 
-    if(name === "" ||
-       email === "" ||
-       phone === "")
-    {
-        alert(
-        "Please fill all mandatory fields"
+        const response = await fetch(
+            "https://sfpctyt404.execute-api.us-east-1.amazonaws.com/prod/apply",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            }
         );
-        return;
+
+        const result = await response.text();
+
+        alert("Application Submitted Successfully!");
+
+        console.log(result);
+
+    } catch(error) {
+
+        console.error(error);
+
+        alert("Error submitting application");
+
     }
-
-    alert(
-    "Application Submitted Successfully"
-    );
-
-    document.getElementById(
-    "jobForm"
-    ).reset();
 }
